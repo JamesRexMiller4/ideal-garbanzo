8dbefa16e53b54d2de977c251ba1c53506f4c8e0
James Miller <james.rex.miller.4@gmail.com>
2020-04-26 14:57:09 -0600

    Remove TODOs, convert app to fat arrow functional component, pass state data down as props to ResultsContainer

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,14 +5,7 @@ import Form from '../Form/Form';
 import ResultsContainer from '../ResultsContainer/ResultsContainer';
 import Footer from '../Footer/Footer';
 
-// TODO: fetch data from API endpoint using useEffect, store in state
-
-// TODO: add function that cleans data, omitting unused values
-
-// TODO: add function that reformats genres to individual strings, and categorizes them
-
-
-function App() {
+const App = () => {
   const [ data, setData ] = useState([])
 
   useEffect(() => {
@@ -50,12 +43,12 @@ function App() {
       setData(cleanedData)
     })
   }, [])
-  
+
   return (
     <div className="App">
       <Header />
       <Form />
-      <ResultsContainer />
+      <ResultsContainer data={data}/>
       <Footer />
     </div>
   );
