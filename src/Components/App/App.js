9f3f4a441b50672ac70d664aba72df44fa096b45
James Miller <james.rex.miller.4@gmail.com>
2020-04-26 10:06:17 -0600

    Import useState, useEffect hooks, add TODO comments

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import Header from '../Header/Header';
 import Form from '../Form/Form';
 import ResultsContainer from '../ResultsContainer/ResultsContainer';
 import Footer from '../Footer/Footer';
+
+// TODO: fetch data from API endpoint using useEffect, store in state
+
+// TODO: add function that cleans data, omitting unused values
+
+// TODO: add function that reformats genres to individual strings, and categorizes them
 
 
 function App() {
