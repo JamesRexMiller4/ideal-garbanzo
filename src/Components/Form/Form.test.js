81963c2090ee2faa3a072e53cdf630cafd9775ca
James Miller <james.rex.miller.4@gmail.com>
2020-04-28 13:24:18 -0600

    Refactor, tidy up, update snapshots

--- src/Components/Form/Form.test.js
+++ src/Components/Form/Form.test.js
@@ -7,31 +7,31 @@ import mockData from '../../data/mockData.js';
 const alphabatizeResults = (results=mockData) => {
   const newResults = [...results]
   return  newResults.sort((a, b) => a.name > b.name ? 1 : -1)
-}
+};
 
 describe('Form', () => {
   let wrapper, mockSetFilteredResults;
   beforeEach(() => {
-    mockSetFilteredResults = jest.fn()
+    mockSetFilteredResults = jest.fn();
     wrapper = mount(<Form
       data={mockData}
       results={alphabatizeResults()}
       setFilteredResults={mockSetFilteredResults}
-      />) 
+      />);
   });
 
   it('should render the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('should render a different snapshot if advancedSearch is true', () => {
-    wrapper.find("#advanced-search-btn").simulate('click')
+  it('handleClick: should render a different snapshot if advancedSearch is true', () => {
+    wrapper.find("#advanced-search-btn").simulate('click');
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('should update state if query is typed', () => {
-    const mockEvent: Object = {target: {value: "Old"}}
-    wrapper.find('#search-bar').simulate('change', mockEvent)
+  it('handleChange: should update state if query is typed', () => {
+    const mockEvent: Object = {target: {value: "Old"}};
+    wrapper.find('#search-bar').simulate('change', mockEvent);
     expect(wrapper.find('#search-bar').getDOMNode().value).toEqual("Old");
-  })
+  });
 });
