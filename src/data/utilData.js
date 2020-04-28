ae42bbc43ca55301bc9d33efb0e2461e63eb5ff8
James Miller <james.rex.miller.4@gmail.com>
2020-04-28 12:34:26 -0600

    Create data folder move utilData into it

---
+++ src/data/utilData.js
@@ -0,0 +1,101 @@
+const utilData = {
+  "states": [
+    "",
+    "Alabama",
+    "Alaska",
+    "Arizona",
+    "Arkansas",
+    "California",
+    "Colorado",
+    "Connecticut",
+    "Delaware",
+    "Florida",
+    "Georgia",
+    "Hawaii",
+    "Idaho",
+    "Illinois",
+    "Indiana",
+    "Iowa",
+    "Kansas",
+    "Kentucky",
+    "Louisiana",
+    "Maine",
+    "Maryland",
+    "Massachusetts",
+    "Michigan",
+    "Minnesota",
+    "Mississippi",
+    "Missouri",
+    "Montana",
+    "Nebraska",
+    "Nevada",
+    "New Hampshire",
+    "New Jersey",
+    "New Mexico",
+    "New York",
+    "North Carolina",
+    "North Dakota",
+    "Ohio",
+    "Oklahoma",
+    "Oregon",
+    "Pennsylvania",
+    "Rhode Island",
+    "South Carolina",
+    "South Dakota",
+    "Tennessee",
+    "Texas",
+    "Utah",
+    "Vermont",
+    "Virginia",
+    "Washington",
+    "West Virginia",
+    "Wisconsin",
+    "Wyoming"
+  ], 
+
+  "Regions": {
+    "Conteninental": {"American": "Hawaiian"},
+    "European": ["Belgian", "British","French", "Irish","Italian"],
+    "International" : {"Asian" : ["Japanese", "Vietnamese"]},
+    "Pacific Rim": "Polynesian"
+  },
+
+  "Food": [
+    "Kosher",
+    "Oysters",
+    "Pasta",
+    "Sandwiches",
+    "Seafood",
+    "Steak",
+    "Sushi",
+    "Vegetarian"
+  ],
+
+  "Types": [
+    "Bakery",
+    "Bistro",
+    "Cafe",
+    "Grill"
+  ],
+
+  "Styles": [
+    "Fusion",
+    "Traditional",
+    "Contemporary",
+    "Eclectic"
+  ],
+
+  "Drinks": [
+    "Coffee",
+    "Tea",
+  ],
+
+  "Atttire": [
+    "casual",
+    "business casual",
+    "smart casual", 
+    'formal'
+  ]
+}
+
+export default utilData;
