3f4047786493b31f2f6a6d53e24595b71d306342
James Miller <james.rex.miller.4@gmail.com>
2020-04-29 17:30:56 -0600

    Add App snapshot, delete unused icons

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,35 +1,14 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { mount } from 'enzyme';
 import App from './App';
 
+
 describe('App', () => {
-  describe('Search Bar', () => {
-    it.skip('should be able to search for a restaurant', () => {
-  
-    });
-  
-    it.skip('should be able to search by name, city, or genre', () => {
-
-    });
-    
-    });
-  
-  describe('Fieldset/Filters', () => {
-    it.skip('should be able to filter by state', () => {
-  
-    });
-  
-    it.skip('should be able to filter by name', () => {
-  
-    });
-  
-    it.skip('should be able to filter by genre', () => {
-      
-    });
-  
-    it.skip('should be able to filter by city', () => {
-
-    });
-    
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(<App />)
+  })
+  it('should render the snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
   });
 });
