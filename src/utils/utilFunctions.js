bc0e1ca19cee3207f1ac6fee1312431e2aeec5af
James Miller <james.rex.miller.4@gmail.com>
2020-04-29 11:00:42 -0600

    Pull out filters into utils and test them

--- src/utils/utilFunctions.js
+++ src/utils/utilFunctions.js
@@ -1,4 +1,18 @@
 export const alphabatizeResults = (results) => {
-  const newResults = [...results]
-  return  newResults.sort((a, b) => a.name > b.name ? 1 : -1)
+  return  results.sort((a, b) => a.name > b.name ? 1 : -1)
 };
+
+export const filterByState = (arr, selectedState, states) => {
+  let selectedAbbrv = states[selectedState];
+  if (!selectedAbbrv) return arr
+  return arr.filter(record => record.state.includes(selectedAbbrv))
+};
+
+export const filterByQuery = (arr, query) => {
+  return arr.filter(record => {
+    return record.name.toLowerCase().includes(query.toLowerCase()) ? true
+    : record.city.toLowerCase().includes(query.toLowerCase()) ? true
+    : record.genre.toLowerCase().includes(query.toLowerCase()) ? true
+    : null
+  })
+}
